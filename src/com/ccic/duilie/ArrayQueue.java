6d34513eff130a8ba3187d751a75b45d68df322f
JacksonHuang2019 <573694655@qq.com>
2019-11-08 09:22:24 +0800

    『并发包入坑指北』之阻塞队列

--- src/com/ccic/duilie/ArrayQueue.java
+++ src/com/ccic/duilie/ArrayQueue.java
@@ -8,6 +8,23 @@ package com.ccic.duilie;
  * @Version ：
  **/
 public class ArrayQueue {
+    // 队列数量
+    private int count;
+
+    // 最终的数据存储
+    private Object[] items;
+
+    // 队列满时的阻塞锁
+    private  Object full = new Object();
+    // 队列空时的阻塞锁
+    private  Object empty = new Object();
+
+    // 写入数据时的下标
+    private int putIndex;
+
+    // 获取数据时的下标
+    private  int getIndex;
+
     public ArrayQueue(int size) {
         items = new Object[size];
     }
