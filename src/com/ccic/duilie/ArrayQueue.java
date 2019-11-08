8c480bef38e90d76a4387913f309ee759a0e2dbf
JacksonHuang2019 <573694655@qq.com>
2019-11-08 09:12:07 +0800

    『并发包入坑指北』之阻塞队列

---
+++ src/com/ccic/duilie/ArrayQueue.java
@@ -0,0 +1,14 @@
+package com.ccic.duilie;
+
+/**
+ * @Author :hzs
+ * @Date :Created in 9:10 2019/11/8
+ * @Description : 自定义了一个类：ArrayQueue
+ * Modified By   :
+ * @Version ：
+ **/
+public class ArrayQueue {
+    public ArrayQueue(int size) {
+        items = new Object[size];
+    }
+}
